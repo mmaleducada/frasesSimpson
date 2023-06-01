b0cd35f08acf880c509bb589f57404afb6bc4808
Emilse Arias <emilsearias@gmail.com>
2023-06-01 18:12:49 -0300

    agregamos la consulta a la api y enviamos la frase al componente frase

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    try {
+      // peticion GET
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes");
+      const dato = await respuesta.json();
+      console.log(respuesta);
+      console.log(dato[0]);
+      setPersonaje(dato[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase />
-        <Button variant="warning" >
-          Obtener frase
-        </Button>
+        <Frase personaje={personaje} />
+        <Button variant="warning">Obtener frase</Button>
       </Container>
     </>
   );
