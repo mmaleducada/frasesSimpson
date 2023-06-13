4f98e10f060be3e47c73a3ec34fb885881716b4b
Malena de Arriba <dearribamalena@gmail.com>
2023-06-13 11:30:17 -0300

    agregue el maquetado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container, Spinner } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
-import { useEffect, useState } from "react";
+
 
 function App() {
-  const [personaje, setPersonaje] = useState({});
-  const [mostrarSpinner, setMostrarSpinner] = useState(true);
-
-  useEffect(() => {
-    consultarAPI();
-  }, []);
-
-  const consultarAPI = async () => {
-    try {
-      setMostrarSpinner(true);
-      // peticion GET
-      const respuesta = await fetch(
-        "https://thesimpsonsquoteapi.glitch.me/quotes");
-      const dato = await respuesta.json();
-      console.log(respuesta);
-      console.log(dato[0]);
-      setPersonaje(dato[0]);
-      setMostrarSpinner(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const componenteRenderizado = (mostrarSpinner)? (<div className="my-5">
-  <Spinner animation="border" variant="primary" />
-  </div>) : <Frase personaje={personaje} />
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        {componenteRenderizado}
-        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
+        <Frase />
+        <Button variant="warning" >
+          Obtener frase
+        </Button>
       </Container>
     </>
   );
