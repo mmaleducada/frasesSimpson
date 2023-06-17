da144655a5012c20fcc09fa17f7949efcce4e32a
Malena de Arriba <dearribamalena@gmail.com>
2023-06-17 20:29:37 -0300

    agregue un spinner con la funcionalidad para que aparezca apenas cargue la pagina y entre cada peticion que realizo al tocar el boton de obtener frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 
 function App() {
   const [personaje, setPersonaje] = useState({}); //aqui voy a guardar la frase que llega, en un objeto vacio {}
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     consultarAPI(); // funcion que se va a encargar de hacer la peticion a la api
@@ -15,11 +16,13 @@ function App() {
 
   const consultarAPI = async () => {
     try {
+      setMostrarSpinner(true);
       const respuesta = await fetch ("https://thesimpsonsquoteapi.glitch.me/quotes")
       const dato = await respuesta.json(); //.json(); metodo que extrae los datos del body y los parsea para leerlos.
       console.log(respuesta);{
       console.log(dato[0]);
       setPersonaje(dato[0]); //cargo el dato en la funcion que renderiza el state
+      setMostrarSpinner(false);
     }
     } catch (error) {
       //aqui puedo mostrar un msj al usuario de que hay un error
@@ -27,11 +30,15 @@ function App() {
     };
   }
 
+  const componenteRenderizado = (mostrarSpinner) ? (<div className="my-5">
+  <Spinner animation="border" variant="danger" />
+  </div>) :  <Frase personaje= {personaje} /> /*mando la funcion por props */
+
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        { <Frase personaje= {personaje} /> /*mando la funcion por props */}
+        {componenteRenderizado}
         <Button variant="warning" onClick={consultarAPI}>
           Obtener frase
         </Button>
